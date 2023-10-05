7ccf34b0febb097e3468cfb0bdcc737e35bdeb10
Tabea Schoeler <51262830+TabeaSchoeler@users.noreply.github.com>
2023-10-05 07:46:31 +0200

    Update UKBBmeasurement.sh

--- analysis/UKBBmeasurement.sh
+++ analysis/UKBBmeasurement.sh
@@ -3,64 +3,61 @@
 ##############################################################################
 
 # ======== EXTRACT PHENOTYPE DATA =============
-hours="4"; task="extractPheno"; array="1" #=> DONE
+hours="4"; task="extractPheno"; array="1"
 submitJob $task $hours
 
 # ======== PROCESS PHENOTYPE DATA =============
-hours="2"; task="processPheno"; array="1" #=> DONE
+hours="2"; task="processPheno"; array="1" 
 submitJob $task $hours
 
 # ======== GENERATE PRINCIPAL COMPONENTS ======
-hours="24"; task="pca"; array="1" #=> DONE
+hours="24"; task="pca"; array="1" 
 submitJob $task $hours
 
 # ======= COMPARE WITH PARTICIPATION ==========
-hours="10"; task="comparePB"; array="1-3" #=> DONE
+hours="10"; task="comparePB"; array="1-3" 
 submitJob $task $hours
 
 # ======= RUN REGENIE =========================
 # step 1
-hours="23"; task="gwaS1"; array="1"; run="pca" #=> DONE
+hours="23"; task="gwaS1"; array="1"; run="pca"
 submitJob $task $hours
 
 # step 2
-hours="23"; task="gwaS2"; array="1-22"; run="pca" #=> DONE
+hours="23"; task="gwaS2"; array="1-22"; run="pca" 
 submitJob $task $hours
 
 # process REGENIE
-hours="2"; task="processREGENIE"; array="1"; run="pca" #=> DONE
+hours="2"; task="processREGENIE"; array="1"; run="pca"
 submitJob $task $hours
 
 
 # ============ Prepare LD scores for Jacknife ==============
-# prepare for jacknife (do only once)
 hours="2"; task="JKldsc"; array="1" #=> DONE
 submitJob $task $hours 
 
 # ============== LD SCORE REGRESSION ==================
-# run ldsc regression on the PCs
-hours="6"; task="rg"; array="1" #=> DONE
+hours="6"; task="rg"; array="1" # run ldsc regression on the PCs
 submitJob $task $hours
 
 # ============ Mendelian Randomization ==================
 # run MR on the PCs
-hours="6"; task="runMR"; array="1"  #=> DONE
+hours="6"; task="runMR"; array="1" 
 submitJob $task $hours 
 
-
 # ======= GWA on error-corrected phenotypes =============
-# step 1 (requires data generated from 'processPheno')
-hours="23"; task="gwaS1"; array="1"; run="gwaCor" #=> DONE
+# step 1 
+hours="23"; task="gwaS1"; array="1"; run="gwaCor" 
 submitJob $task $hours
 
 # step 2
-hours="47"; task="gwaS2"; array="1-22"; run="gwaCor" #=> done
+hours="47"; task="gwaS2"; array="1-22"; run="gwaCor" 
 submitJob $task $hours
 
 # process REGENIE
 numPheno=$(wc -l "$HOME/data/gwas/gwaCorr")
 $numPheno
-hours="2"; task="processREGENIE"; array="1-23"; run="gwaCorr" #=> done
+hours="2"; task="processREGENIE"; array="1-23"; run="gwaCorr" 
 submitJob $task $hours #=> running
 
 # get Jacknife estimates
